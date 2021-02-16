1574a3df49686be6b65822493d87055b24ef76cc
KatieSylvester <katie.r.sylvester@gmail.com>
2021-02-16 09:14:00 -0600

    final analysis

--- as_content_rating_app_trader.sql
+++ as_content_rating_app_trader.sql
@@ -1,16 +1,7 @@
-
-/* OPTION 1
-approach: only looks at the apps that appear in both stores, since that is client's preference 
-downside: possibly filters out a great option that only appear in one store
-challenges left: we haven't considered where in the life cycle of the app these apps are
-deliverables: 
-1. Develop some general recommendations as to the price range, genre, content rating, 
-or anything else for apps that the company should target
-2. Develop a Top 10 List of the apps that App Trader should buy next week for its Black Friday debut.
-3. Prepare a 5-10 minute presentation for the leadership team of App Trader*/ 
+/* final results -- avg lifespan profit grouped by app store content rating */ 
 
 -- the names cte limits the query results to apps that appear in BOTH stores (there are 328)
-WITH names AS (SELECT DISTINCT(name)
+WITH name AS (SELECT DISTINCT(name)
 			FROM play_store_apps
 			INNER JOIN app_store_apps
 			USING (name)),
@@ -20,7 +11,12 @@ WITH names AS (SELECT DISTINCT(name)
 -- the expected total life span of the app, based on the app rating
 -- all for just the play store apps
 psa_calculations AS (
-	SELECT name AS name, 
+	SELECT DISTINCT(name) AS name, 
+	genres,
+	--category,
+	content_rating,
+	--review_count,
+	install_count,
 	CAST(rating AS FLOAT) AS ps_rating, 
 	CAST(replace(price,'$','')AS FLOAT) AS ps_price,
 	CASE WHEN CAST(replace(price,'$','')AS FLOAT) <=1 THEN 10000
@@ -44,7 +40,10 @@ psa_calculations AS (
 -- the expected total life span of the app, based on the app rating
 -- all for just the app store apps
 asa_calculations AS (
-	SELECT name AS name, 
+	SELECT DISTINCT(name) AS name, 
+	--primary_genre,
+	content_rating,
+	--review_count,
 	CAST(rating AS FLOAT) AS as_rating, 
 	CAST(price AS FLOAT) AS as_price,
 	CASE WHEN CAST(price AS FLOAT) <=1 THEN 10000
@@ -66,25 +65,35 @@ asa_calculations AS (
 -- names cte to limit the results to the 328 apps that appear in both stores
 -- also creates a play_store_app profit column, an app_store_app profit column,
 -- a marketing cost column and a total profit column
-SELECT DISTINCT(names.name),
-psa.ps_price,
-psa.ps_initial_cost,
-psa.ps_rating,
-psa.ps_life_span_yr,
-(2500*12*psa.ps_life_span_yr - psa.ps_initial_cost) AS psa_profit,
-asa.as_price,
-asa.as_initial_cost,
-asa.as_rating,
-asa.as_life_span_yr,
-(2500*12*asa.as_life_span_yr - asa.as_initial_cost) AS asa_profit,
-CASE WHEN as_life_span_yr > ps_life_span_yr THEN 1000*as_life_span_yr
-ELSE 1000*12*ps_life_span_yr END AS marketing_cost,
-((2500*12*psa.ps_life_span_yr - psa.ps_initial_cost) + (2500*12*asa.as_life_span_yr - asa.as_initial_cost) - 
-CASE WHEN as_life_span_yr > ps_life_span_yr THEN 1000*12*as_life_span_yr ELSE 1000*12*ps_life_span_yr END) AS total_life_span_profit
-FROM names
+SELECT --DISTINCT(name.name),
+--psa.genres,
+--psa.content_rating,
+AVG(psa.ps_price) AS avg_ps_price,
+--AVG(psa.ps_initial_cost) AS ps_avg_cost,
+--psa.category,
+--psa.review_count,
+--AVG(psa.install_count::NUMERIC),
+--psa.ps_rating,
+--psa.ps_life_span_yr,
+AVG((2500*12*psa.ps_life_span_yr - psa.ps_initial_cost)) AS psa_avg_profit,
+AVG(asa.as_price) AS as_avg_price,
+--AVG(asa.as_initial_cost) AS as_avg_cost,
+--asa.primary_genre,
+asa.content_rating,
+--asa.review_count,
+--asa.as_rating,
+--asa.as_life_span_yr,
+AVG((2500*12*asa.as_life_span_yr - asa.as_initial_cost)) AS asa_avg_profit,
+--ROUND(AVG(CASE WHEN as_life_span_yr > ps_life_span_yr THEN 1000*12*as_life_span_yr
+--ELSE 1000*12*ps_life_span_yr END),2) AS marketing_cost,
+AVG((2500*12*psa.ps_life_span_yr - psa.ps_initial_cost) + (2500*12*asa.as_life_span_yr - asa.as_initial_cost) - 
+CASE WHEN as_life_span_yr > ps_life_span_yr THEN 1000*12*as_life_span_yr ELSE 1000*12*ps_life_span_yr END) AS avg_total_life_span_profit
+FROM name
 LEFT JOIN psa_calculations AS psa
 USING (name)
 LEFT JOIN asa_calculations AS asa
 USING (name)
-ORDER BY total_life_span_profit DESC;
-		
+GROUP BY asa.content_rating
+ORDER BY AVG((2500*12*psa.ps_life_span_yr - psa.ps_initial_cost) + (2500*12*asa.as_life_span_yr - asa.as_initial_cost) - 
+CASE WHEN as_life_span_yr > ps_life_span_yr THEN 1000*12*as_life_span_yr ELSE 1000*12*ps_life_span_yr END) DESC
+LIMIT 10;
